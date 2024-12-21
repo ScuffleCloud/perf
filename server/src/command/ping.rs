e8f03e9658d9b2290efb7a8f957628d50b58da9b
Troy Benson <troybensonsa@gmail.com>
2024-12-21 05:35:58 +0000

    add ping test

--- server/src/command/ping.rs
+++ server/src/command/ping.rs
@@ -26,3 +26,99 @@ pub async fn handle<R: GitHubRepoClient>(
 
     Ok(())
 }
+
+#[cfg(test)]
+#[cfg_attr(coverage_nightly, coverage(off))]
+mod tests {
+    use std::sync::Arc;
+
+    use octocrab::models::UserId;
+
+    use super::*;
+    use crate::database::get_test_connection;
+    use crate::github::merge_workflow::GitHubMergeWorkflow;
+    use crate::github::models::{PullRequest, User};
+    use crate::github::repo::test_utils::{MockRepoAction, MockRepoClient};
+
+    struct MockMergeWorkFlow;
+
+    impl GitHubMergeWorkflow for MockMergeWorkFlow {}
+
+    #[tokio::test]
+    async fn test_ping_enabled() {
+        let mut conn = get_test_connection().await;
+        let (client, mut rx) = MockRepoClient::new(MockMergeWorkFlow);
+
+        tokio::spawn(async move {
+            handle(
+                &mut conn,
+                BrawlCommandContext {
+                    repo: &client,
+                    pr: Arc::new(PullRequest::default()),
+                    user: User {
+                        id: UserId(1),
+                        login: "troy".to_string(),
+                    },
+                },
+            )
+            .await
+            .unwrap();
+        });
+
+        match rx.recv().await.unwrap() {
+            MockRepoAction::SendMessage {
+                issue_number,
+                message,
+                result,
+            } => {
+                assert_eq!(issue_number, 0);
+                insta::assert_snapshot!(message, @r"
+                Pong, @troy!
+                Brawl is currently enabled on this repo.
+                ");
+                result.send(Ok(())).unwrap();
+            }
+            _ => panic!("expected send message event"),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_ping_disabled() {
+        let mut conn = get_test_connection().await;
+        let (mut client, mut rx) = MockRepoClient::new(MockMergeWorkFlow);
+
+        client.config.enabled = false;
+
+        tokio::spawn(async move {
+            handle(
+                &mut conn,
+                BrawlCommandContext {
+                    repo: &client,
+                    pr: Arc::new(PullRequest::default()),
+                    user: User {
+                        id: UserId(1),
+                        login: "troy".to_string(),
+                    },
+                },
+            )
+            .await
+            .unwrap();
+        });
+
+        match rx.recv().await.unwrap() {
+            MockRepoAction::SendMessage {
+                issue_number,
+                message,
+                result,
+            } => {
+                assert_eq!(issue_number, 0);
+                insta::assert_snapshot!(message, @r"
+                Pong, @troy!
+                Brawl is currently disabled on this repo.
+                ");
+                result.send(Ok(())).unwrap();
+            }
+            _ => panic!("expected send message event"),
+        }
+    }
+}
